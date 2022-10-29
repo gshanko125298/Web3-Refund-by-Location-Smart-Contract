af5fb7372eba23be71805b5fda8aa187001c581c
gshanko125298 <gdekebo2020@gmail.com>
2022-10-29 22:09:04 +0300

    new Update

---
+++ my-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
